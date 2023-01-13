139f3685d4a2b3a82abd657e04b48bcc99688153
jsaique <jsaique11@gmail.com>
2023-01-12 20:39:25 -0500

    Experimenting with design

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,9 @@
-'use strict';
+import Typed from 'typed.js';
 
-const header = document.querySelector('header');
-const menuToggle = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
-const darkmode = document.querySelector('.darkmode-toggle');
-
-// Scrolling
-
-window.onscroll = () => {
-  navbar.classList.remove('active');
-};
-
-window.addEventListener('scroll', () => {
-  header.classList.toggle('shadow', window.scrollY > 0);
+const typed = new Typed('.multi-text', {
+  strings: ['John', 'a Coder', 'a Developer'],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1500,
 });
-
-// Navbar
-menuToggle.addEventListener('click', function () {
-  navbar.classList.toggle('active');
-});
-
-// Darkmode
-darkmode.onclick = () =>
-  darkmode.classList.contains('bx-moon')
-    ? darkmode.classList.replace('bx-moon', 'bx-sun') &&
-      document.body.classList.add('active')
-    : darkmode.classList.replace('bx-sun', 'bx-moon') &&
-      document.body.classList.remove('active');
